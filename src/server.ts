c4b31a66a7a70a23cdd502c1e6c41a2e1fbb6e94
Márcio Rodrigues <81385414+marciomrcr@users.noreply.github.com>
2022-11-04 18:12:56 -0300

    Feature: create function create pools

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
-
 import Fastify from "fastify";
+import ShortUniqueId from "short-unique-id";
+import { z } from "zod";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -16,10 +17,48 @@ async function bootstrap() {
     origin: true,
   });
 
-  fastify.get("/clientes", async () => {
-    const client = await prisma.cliente.findMany();
+  fastify.get("/clientes/count", async () => {
+    const count = await prisma.cliente.count();
 
-    return { client };
+    return { count };
+  });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ });
