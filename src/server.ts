c6d97b1bf4f879f8402db71054d075a67d2d455c
Márcio Rodrigues <81385414+marciomrcr@users.noreply.github.com>
2022-11-04 15:16:42 -0300

    db structure Pools

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,25 @@ import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
 
 const prisma = new PrismaClient({
-  log: ['query']
-})
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
-    logger: true
-  })
+    logger: true,
+  });
 
   await fastify.register(cors, {
-    origin: true
-  })
+    origin: true,
+  });
 
-  fastify.get('/clientes', async () => {    
-   
-    const client = await prisma.cliente.findMany()
+  fastify.get("/clientes", async () => {
+    const client = await prisma.cliente.findMany();
 
-    return{ client}
-  })
+    return { client };
+  });
 
-  await fastify.listen({port: 3333, host: '0.0.0.0'})  
+  await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ });
 }
 
-bootstrap()
+bootstrap();
